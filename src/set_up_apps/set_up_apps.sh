85eba385aeb91689c303ffca019fac3d79ee0453
arjunsudheer <arjunsudheer05@outlook.com>
2024-06-23 13:46:25 -0700

    Refactor set_up_apps.sh
    Create functions to open apps from a file or from the command line, remove user prompts

--- src/set_up_apps/set_up_apps.sh
+++ src/set_up_apps/set_up_apps.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
-openApps() {
-    IFS=""
-    local appsOptions=()
-    local appsCount=0
-    while read -r line; do
-        appsOptions+=($line)
-        (( appsCount++ ))
-    done < personal_automation_info/set_up_apps.txt
-    appsOptions+=("go back")
-    appsOptions+=("exit")
-    
-    promptUser "${appsOptions[@]}" 
-
-    read -r -p "Which app would you like to open: " appChoice
-
-    if [[ $($appChoice == "go back" 2>/dev/null) || $($appChoice -eq $(( appsCount + 1 )) 2>/dev/null) ]]; then
-        return
-    elif [[ $($appChoice == "exit" 2>/dev/null) || $($appChoice -eq $(( appsCount + 2  )) 2>/dev/null) ]]; then
-        exit
-    # if the user types a number, then map it to the appropriate app name
-    elif [[ $appChoice =~ ^[+-]?[0-9]+$ ]]; then
-        # check to see if the inputted number is less than or equal to fileOrDirectoryCount to make sure it is a valid option
-        if [[ $appChoice -le $appsCount && $appChoice -gt 0 ]]; then
-            # if the user is using MacOS, use the open command, otherwise, run the Linux command of xdg-open
-            if [[ $OSTYPE == "darwin"* ]]; then
-                open -a ${appsOptions[$(( $appChoice - 1 ))]}
-            else
-                xdg-open ${appsOptions[$(( $appChoice - 1 ))]}
-            fi
-        else
-            echo -e "\nInvalid selection. If you want to open a new file or directory, please make sure that the name and path are added to the \"set_up_apps.txt\" file."
-            echo -e "This is what your \"set_up_apps.txt\" file has right now.\n"
-            cat personal_automation_info/set_up_apps.txt
-            echo -e "\n"
-        fi
-    # if the user types the name of the app instead, then just use the app name
+# Opens the app specified as the argument
+openApp() {
+    if [[ $OSTYPE == "darwin"* ]]; then
+        open -a $1
     else
-    # if the user is using MacOS, use the open command, otherwise, run the Linux command of xdg-open
-        if [[ $OSTYPE == "darwin"* ]]; then
-            open -a $appChoice
-        else
-            xdg-open $appChoice
-        fi
+        xdg-open $1
     fi
 }
 
-# navigate to the project directory
-cd $(< .project_path.txt)
+# Opens all the apps listed in the file specified by the user as an argument
+openAllApps() {
+    IFS=""
+    # If a file was not specified, then list the available app options for the user
+    if [[ -z $1 ]]
+    then
+        readarray -t appOptions < <(ls ../../personal_automation_info/apps/)
+        select file in ${appOptions[@]}
+        do
+            appFile="../../personal_automation_info/apps/$file"
+            break
+        done
+    else
+        appFile="../../personal_automation_info/apps/$1"
+    fi
+    # Open all the apps specified in the file
+    if [[ -f $appFile ]]
+    then
+        while read -r line; do
+            openApp $line
+            sleep 1
+        done < $appFile
+    else
+        echo "Not a valid file"
+    fi
+}
+
+# Allows the user to create a file that specifies a list of apps to be opened
+createAppsList() {
+    # Specify a default value of "apps_list" if a filename is not specified
+    read -e -p "Please enter the name of your apps list file (omit the file extension) [apps_list]: " -i "apps_list" appsListFile
+    read -p "Please enter the name of the app you want to open. Type 'quit' to stop: " application
+    while [[ ! -z $application && $application != "quit" ]]
+    do
+        # Only add the application if a non-empty string was passed
+        echo $application >> ../../personal_automation_info/apps/$appsListFile.txt
+        read -p "Please enter the name of the app you want to open. Type 'quit' to stop: " application
+    done
+}
