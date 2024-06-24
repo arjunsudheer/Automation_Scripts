8804eba82264bf2ac81fc5979978f90c3f1b0f0e
arjunsudheer <arjunsudheer05@outlook.com>
2024-06-24 10:35:01 -0700

    Refactor set_up_files_and_directories.sh
    Create functions to open files and directories in a specified app from a file or from the command line, remove user prompts

--- src/set_up_files_and_directories/set_up_files_and_directories.sh
+++ src/set_up_files_and_directories/set_up_files_and_directories.sh
@@ -1,56 +1,74 @@
 #!/bin/bash
 
+# Opens the file or directory specified as the argument
 openFilesAndDirectories() {
-    IFS=","
-    local filesOrDirectoriesOptions=()
-    local fileOrDirectoryPaths=()
-    local appName=()
-    local fileOrDirectoryCount=0
-    # get the files or directories that the user wants to open, ignoring the first line in the file
-    while read -r name path app; do
-        filesOrDirectoriesOptions+=($name)
-        fileOrDirectoryPaths+=($path)
-        appNames+=($app)           
-        (( fileOrDirectoryCount++ ))
-    done < <(tail -n +2 personal_automation_info/set_up_files_and_directories.csv)
-    filesOrDirectoriesOptions+=("go back")
-    filesOrDirectoriesOptions+=("exit")
-    
-    promptUser "${filesOrDirectoriesOptions[@]}" 
-
-    echo -e "\nNote: Typing the file's or directory's absolute path by hand will open the file or directory in its default application. If you want the file or directory to open in the specified application, then please type the corresponding number instead."    
-    read -r -p "Which file or directory would you like to open: " fileOrDirectoryChoice
-
-    if [[ $($fileOrDirectoryChoice == "go back" 2>/dev/null) || $($fileOrDirectoryChoice -eq $(( fileOrDirectoryCount + 1 )) 2>/dev/null) ]]; then
-        return
-    elif [[ $($fileOrDirectoryChoice == "exit" 2>/dev/null) || $($fileOrDirectoryChoice -eq $(( fileOrDirectoryCount + 2 )) 2>/dev/null) ]]; then
-        exit
-    # if the user types a number, then map it to the appropriate app name
-    elif [[ $fileOrDirectoryChoice =~ ^[+-]?[0-9]+$ ]]; then
-        # check to see if the inputted number is less than or equal to fileOrDirectoryCount to make sure it is a valid option
-        if [[ $fileOrDirectoryChoice -le $fileOrDirectoryCount && $fileOrDirectoryChoice -gt 0 ]]; then
-            # if the user is using MacOS, use the open command, otherwise, run the Linux command of xdg-open
-            if [[ $OSTYPE == "darwin"* ]]; then
-                open -a ${appNames[$(( $fileOrDirectoryChoice - 1 ))]} ${fileOrDirectoryPaths[$(( $fileOrDirectoryChoice - 1 ))]}
-            else
-                xdg-open ${appNames[$(( $fileOrDirectoryChoice - 1 ))]} ${fileOrDirectoryPaths[$(( $fileOrDirectoryChoice - 1 ))]}
-            fi
-        else
-            echo -e "\nInvalid selection. If you want to open a new file or directory, please make sure that the name and path are added to the \"set_up_files_or_directories.csv\" file."
-            echo -e "This is what your \"set_up_files_or_directories.csv\" file has right now.\n"
-            cat personal_automation_info/set_up_files_or_directories.csv
-            echo -e "\n"
-        fi
-    # if the user types the name of the app instead, then just use the app name
-    else
+    # Set IFS to the empty string so files and directories that contain spaces in their names can be opened
+    IFS=""
     # if the user is using MacOS, use the open command, otherwise, run the Linux command of xdg-open
-        if [[ $OSTYPE == "darwin"* ]]; then
-            open $fileOrDirectoryChoice
+    if [[ $OSTYPE == "darwin"* ]]
+    then
+        # If an app was specified, then open the file or directory using that app, otherwise, use the default app
+        if [[ -z $2 ]]
+        then
+            open -u $1
         else
-            xdg-open $fileOrDirectoryChoice
+            open -a $2 $1
+        fi
+    else
+        # If an app was specified, then open the file or directory using that app, otherwise, use the default app
+        if [[ -z $2 ]]
+        then
+            xdg-open $1
+        else
+            xdg-open $2 $1
         fi
     fi
 }
 
-# navigate to the project directory
-cd $(< .project_path.txt)
+# Opens all the files and directories listed in the file specified by the user as an argument
+openAllFilesAndDirectories() {
+    # If a file was not specified, then list the available app options for the user
+    if [[ -z $1 ]]
+    then
+        readarray -t fileAndDirectoryOptions < <(ls ../../personal_automation_info/file_and_directory/)
+        select file in ${fileAndDirectoryOptions[@]}
+        do
+            fdFile="../../personal_automation_info/file_and_directory/$file"
+            break
+        done
+    else
+        fdFile="../../personal_automation_info/file_and_directory/$1"
+    fi
+    # Open all the files and directories specified in the file
+    if [[ -f $fdFile ]]
+    then
+        IFS=","
+        # get the url's that the user wants to open, ignoring the first line in the file
+        while read -r path app; do
+            openFilesAndDirectories $path $app
+            # Reset IFS to the comma delimter since IFS is changed in the openFilesAndDirectories function
+            IFS=","
+            sleep 1
+        done < $fdFile
+    else
+        echo "Not a valid file"
+    fi
+}
+
+# Allows the user to create a file that specifies a list of files and directories to be opened
+createFileAndDirectoryList() {
+    # Set IFS to the empty string so files and directories that contain spaces in their names can be opened
+    IFS=""
+    # Specify a default value of "file_and_directory_list" if a filename is not specified
+    read -e -p "Please enter the name of your file and directory list file (omit the file extension) [file_and_directory_list]: " -i "file_and_directory_list" fdListFile
+    
+    read -p "Please enter the path to the file or directory you want to open: " fdPath
+    read -p "Please enter the app that you want to open the file or directory in. Type 'quit' to stop: " fdApp
+    while [[ ! -z $fdPath && ! -z $fdApp && $fdApp != "quit" ]]
+    do
+        # Only add the file or directory if a non-empty string was passed
+        echo $fdPath,$fdApp >> ../../personal_automation_info/file_and_directory/$fdListFile.csv
+        read -p "Please enter the path to the file or directory you want to open: " fdPath
+        read -p "Please enter the app that you want to open the file or directory in. Type 'quit' to stop: " fdApp
+    done
+}
