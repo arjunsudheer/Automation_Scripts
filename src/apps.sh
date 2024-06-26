b1b390d9f1ef1016ec8ef3bf6a3315ef3830c7c9
arjunsudheer <arjunsudheer05@outlook.com>
2024-06-26 12:17:11 -0700

    Rename files
    Remove redundant checks for arguments

--- src/apps.sh
+++ src/apps.sh
@@ -12,21 +12,14 @@ openApp() {
 # Opens all the apps listed in the file specified by the user as an argument
 openAllApps() {
     IFS=""
-    # If a file was not specified, then list the available app options for the user
-    if [[ -z $1 ]]
-    then
-        readarray -t appOptions < <(ls ../../personal_automation_info/apps/)
-        select file in ${appOptions[@]}
-        do
-            appFile="../../personal_automation_info/apps/$file"
-            break
-        done
-    else
-        appFile="../../personal_automation_info/apps/$1"
-    fi
+    readarray -t appOptions < <(ls ../../personal_automation_info/apps/)
+    select file in ${appOptions[@]}; do
+        appFile="../../personal_automation_info/apps/$file"
+        break
+    done
+    
     # Open all the apps specified in the file
-    if [[ -f $appFile ]]
-    then
+    if [[ -f $appFile ]]; then
         while read -r line; do
             openApp $line
             sleep 1
@@ -41,8 +34,7 @@ createAppsList() {
     # Specify a default value of "apps_list" if a filename is not specified
     read -e -p "Please enter the name of your apps list file (omit the file extension) [apps_list]: " -i "apps_list" appsListFile
     read -p "Please enter the name of the app you want to open. Type 'quit' to stop: " application
-    while [[ ! -z $application && $application != "quit" ]]
-    do
+    while [[ ! -z $application && $application != "quit" ]]; do
         # Only add the application if a non-empty string was passed
         echo $application >> ../../personal_automation_info/apps/$appsListFile.txt
         read -p "Please enter the name of the app you want to open. Type 'quit' to stop: " application
